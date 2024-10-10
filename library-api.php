679d3dd866e85722bcb2aaec826db8b8742c18b2
ashique <ashique12009@gmail.com>
2024-10-10 23:47:55 +0300

    class added for rest routes and controller

--- library-api.php
+++ library-api.php
@@ -10,19 +10,27 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
+// Main plugin class
 class LibraryAPI {
 
     // Plugin Constructor
     public function __construct() {
+        $this->plugin_constants();
         add_action('rest_api_init', [$this, 'register_rest_routes']);
+    }
+
+    // Define constants
+    public function plugin_constants() {
+        define('LIBRARY_API_VERSION', '1.0');
+        define('LIBRARY_API_PLUGIN_DIR', plugin_dir_path(__FILE__));
+        define('LIBRARY_API_PLUGIN_URL', plugin_dir_url(__FILE__));
     }
 
     // Register rest routes
     public function register_rest_routes() {
-        register_rest_route('library/v1', 'library', [
-            'methods' => 'GET',
-            'callback' => [$this, 'get_library']
-        ]);
+        require_once LIBRARY_API_PLUGIN_DIR . 'includes/class-api-controller.php';
+        $apiController = new ClassApiController();
+        $apiController->register_rest_routes();
     }
 }
 
