d31e4b17aff77885de0fd07954576cdf64fa45f0
ashique <ashique12009@gmail.com>
2024-10-11 07:30:26 +0300

    routes controller added

---
+++ includes/class-api-controller.php
@@ -0,0 +1,14 @@
+<?php 
+// API controller class
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+class ClassApiController {
+
+    // Register rest routes
+    public function register_rest_routes() {
+        
+    }
+}
